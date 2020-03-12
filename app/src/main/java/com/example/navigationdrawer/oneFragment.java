32ccdb6df098052a8a37d11b97e1e5ddeb2c1620
Simran <dhamikaran415@gmail.com>
2020-03-12 16:00:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/navigationdrawer/oneFragment.java
@@ -0,0 +1,41 @@
+package com.example.navigationdrawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+public class oneFragment extends Fragment {
+
+
+
+
+    public oneFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        }
+
+}
---
+++ app/src/main/java/com/example/navigationdrawer/thirdFragment.java
@@ -0,0 +1,37 @@
+package com.example.navigationdrawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+public class thirdFragment extends Fragment {
+
+    public thirdFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+
+    }
+}
